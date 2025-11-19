2e38cc10a3ded25f064353258ed271d6677b0873
Nilesh_Shirude <shirudenileshs@gmail.com>
2025-11-19 18:39:27 +0530

    update countrieslist and country card using props

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -1,23 +1,38 @@
 import countryData from "../countryData";
 import CountryCard from "./CountryCard";
-import React from 'react'
+import React from "react";
 
 const CountriesList = () => {
-    
-    const arrays=countryData.map((country)=>{
-        return <CountryCard name={country.name.common}/>
-    })
-    
-  return (
-    <div className="country-container">
-      
-        <CountryCard /> 
-        {/* <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard /> */}
-    </div>
-  )
-}
+  console.log(countryData);
+  // const array=[
+  //     <CountryCard />,
+  //     <CountryCard />,
+  //     <CountryCard />,
+  //     <CountryCard />,
+  //     <CountryCard />,
+  // ]
+  // const array=countryData.map((country)=>{
+  //     console.log(country)
+  //     return (<CountryCard  />)
+  // })
+  // const array=countryData.map((country)=>{
+  //     return (<CountryCard name={country.name.common} />)
+  // })
+  // console.log(array);
+  const array = countryData.map((country) => {
+    // console.log(country);
+    return (
+      <CountryCard
+        key={country.name.common}
+        name={country.name.common}
+        flag={country.flags.svg}
+        population={country.population.toLocaleString("en-IN")}
+        region={country.region}
+        capital={country.capital[0]}
+      />
+    );
+  });
+  return <div className="country-container">{array}</div>;
+};
 
-export default CountriesList
+export default CountriesList;
--- components/CountryCard.jsx
+++ components/CountryCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const CountryCard = ({props}) => {
-    console.log(props);
+const CountryCard = ({name,flag,population,region,capital}) => {
+    console.log(name);
   return (
-    <a className="country-card" href="/country.html?name=Syria"><img src="https://flagcdn.com/sy.svg" alt="flag" />
+    <a className="country-card" href="/country.html?name=Syria"><img src={flag} alt="flag" />
         <div className="country-text">
-            <h3 className="capital">Syria</h3>
-            <p><b>Population:</b>25620000</p>
-            <p><b>Region:</b>Asia</p>
-            <p><b>Capital:</b>Damascus</p>
+            <h3 className="capital">{name}</h3>
+            <p><b>Population:</b>{population}</p>
+            <p><b>Region:</b>{region}</p>
+            <p><b>Capital:</b>{capital}</p>
         </div>
     </a>
   )
