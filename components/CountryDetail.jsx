fc8c40fb6a4b2577fc0ac01c7a1e50325a39483b
Nilesh_Shirude <shirudenileshs@gmail.com>
2025-11-22 18:52:49 +0530

    routing react rout

--- components/CountryDetail.jsx
+++ components/CountryDetail.jsx
@@ -1,13 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import './CountryDetail.css'
 
 const CountryDetail = () => {
   const countryName = new URLSearchParams(window.location.search).get("name");
+  const [countryData,setCountryData]=useState({})
   console.log(countryName);
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
       .then((res) => res.json())
-      .then((data) => {
+      .then(([data]) => {
         console.log(data);
+        setCountryData({
+          name:data.name.common,
+          nativeName:Object.values(data.name.nativeName)[0].common,
+          population:data.population.toLocaleString('en-IN'),
+          region:data.region,
+          subRegion:data.subregion,
+          capital:data.capital[0],
+          tld:data.tld[0],
+          currencies:Object.values(data.currencies).map((currency)=>currency.name).join(', '),
+          languages:Object.values(data.languages).join(', '),
+          flag:data.flags.svg
+        })
+       
       });
   },[]);
   return (
@@ -16,40 +31,40 @@ const CountryDetail = () => {
         <i className="fa-solid fa-arrow-left"></i>&nbsp;Back
       </span>
       <div className="country-details">
-        <img src={null} alt="flag" />
+        <img src={countryData.flag} alt={`${countryData.name}flag`} />
         <div className="country-details-container">
-          <h1></h1>
+          <h1>{countryData.name}</h1>
           <div className="country-details-text">
             <p>
-              <b>Native Name:</b>
+              <b>Native Name:{countryData.nativeName}</b>
               <span className="native-name"></span>
             </p>
             <p>
-              <b>Population:</b>
+              <b>Population:{countryData.population}</b>
               <span className="country-population"></span>
             </p>
             <p>
-              <b>Region:</b>
+              <b>Region:{countryData.region}</b>
               <span className="country-region"></span>
             </p>
             <p>
-              <b>Sub Region:</b>
+              <b>Sub Region:{countryData.subRegion}</b>
               <span className="country-sub-region"></span>
             </p>
             <p>
-              <b>Capital:</b>
+              <b>Capital:{countryData.capital}</b>
               <span className="country-capital"></span>
             </p>
             <p>
-              <b>Top Level Domain:</b>
+              <b>Top Level Domain:{countryData.tld}</b>
               <span className="country-tld"></span>
             </p>
             <p>
-              <b>Currencies:</b>
+              <b>Currencies:{countryData.currencies}</b>
               <span className="country-currencies"></span>
             </p>
             <p>
-              <b>Languages:</b>
+              <b>Languages:{countryData.languages}</b>
               <span className="country-languages"></span>
             </p>
           </div>
